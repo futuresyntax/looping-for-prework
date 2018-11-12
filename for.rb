55d043c14aafb0f6789e9e1ea350a75e4a7bd98c
FutureScript <fernandobaxter1@gmail.com>
2018-11-12 02:13:32 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -1,7 +1,12 @@
+# 
+# def using_for
+# 	for checklist 1..10 do
+# 	puts "Wingardium Leviosa"
+# end
 
 def using_for
-	for checklist 1..10 do
-	puts "Wingardium Leviosa"
-end
+	for checklist 1..10 do 
+		puts "Wingardium Leviosa"
+	end 
 
 
